7ed14c464482a14ec5d0b6593e164c1a50037dc3
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-01-23 02:53:28 +0100

    Add getProducts call in App

--- src/App.js
+++ src/App.js
@@ -9,31 +9,35 @@ import {
 import IndexPage from "./pages/IndexPage/IndexPage";
 import initializeServer from "./server/index";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-
-import storylineData from "./server/storylineData.js";
 import ReadPage from "./pages/ReadPage/ReadPage";
+import useProducts from "./hooks/useProducts";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 initializeServer();
 
 library.add(faChevronLeft, faFilter, faTimes, faEdit);
 
 function App() {
+  const { getProducts } = useProducts();
+  const { products } = useSelector((store) => store);
+
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/products" />} />
-      <Route
-        path="/products"
-        element={<IndexPage productsList={storylineData} />}
-      />
+      <Route path="/products" element={<IndexPage productsList={products} />} />
       <Route
         path="/products/:idProduct"
-        element={<ReadPage productsList={storylineData} />}
+        element={<ReadPage productsList={products} />}
       />
     </Routes>
   );
