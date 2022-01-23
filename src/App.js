e77ddf936217f6c2037b882585184af0361219cb
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-01-23 01:16:51 +0100

    Add ReadPage page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faChevronLeft, faFilter } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronLeft,
+  faFilter,
+  faTimes,
+  faEdit,
+} from "@fortawesome/free-solid-svg-icons";
 import IndexPage from "./pages/IndexPage/IndexPage";
 import initializeServer from "./server/index";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import storylineData from "./server/storylineData.js";
+import ReadPage from "./pages/ReadPage/ReadPage";
 
 initializeServer();
 
-library.add(fab, faChevronLeft, faFilter);
+library.add(fab, faChevronLeft, faFilter, faTimes, faEdit);
 
 function App() {
   return (
@@ -19,6 +25,10 @@ function App() {
       <Route
         path="/products"
         element={<IndexPage productsList={storylineData} />}
+      />
+      <Route
+        path="/products/:idProduct"
+        element={<ReadPage productsList={storylineData} />}
       />
     </Routes>
   );
