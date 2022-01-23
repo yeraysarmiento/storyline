8625642eacb9f6648f2bf8123d40aa6af9ac263e
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-01-23 12:10:34 +0100

    Added navigation to Menu buttons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import logo from "../../assets/img/logo.png";
 import "./Menu.css";
 
 function Menu() {
   let { pathname } = useLocation();
+  const navigate = useNavigate();
 
   return (
     <header className="menu">
-      <h1 className="heading">
-        <img src={logo} alt="Logo of Storyline Shop" width="36" height="36" />
-      </h1>
+      <Link to="/products">
+        <h1 className="heading">
+          <img src={logo} alt="Logo of Storyline Shop" width="36" height="36" />
+        </h1>
+      </Link>
       {pathname !== "/products" && (
         <div className="menu__element">
           <FontAwesomeIcon className="menu__arrow" icon="chevron-left" />
-          <Link to="/products">
-            <p className="menu__goback">Go Back</p>
-          </Link>
+          <button onClick={() => navigate(-1)} className="menu__goback">
+            Go Back
+          </button>
         </div>
       )}
     </header>
