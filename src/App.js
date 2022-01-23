80073ae43000fea769771618c4166d2a3b3c3344
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-01-23 01:27:38 +0100

    Added auto scroll to top

--- src/App.js
+++ src/App.js
@@ -9,16 +9,22 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import IndexPage from "./pages/IndexPage/IndexPage";
 import initializeServer from "./server/index";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
 import storylineData from "./server/storylineData.js";
 import ReadPage from "./pages/ReadPage/ReadPage";
+import { useEffect } from "react";
 
 initializeServer();
 
 library.add(fab, faChevronLeft, faFilter, faTimes, faEdit);
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/products" />} />
